b4f870689d0bd32d369614e11c7c1db25e105b89
Yakondra <140064177+Yakondra@users.noreply.github.com>
2023-10-24 23:35:04 +0000

    exercise terminal Alexandra

--- src/main.py
+++ src/main.py
@@ -1,2 +1,45 @@
 # Obligatorio: Generar una tabla usando Python con TODOS los ficheros (recursivamente) del workspace que contenga el nombre del fichero, el peso REAL y la última fecha de modificación.
+import os
+import pandas as pd
+from datetime import datetime
+
+main_root = '/workspaces/exercise-terminal-challenge'
+
+
+def lista_archivos(ruta):
+    archivos = []
+    for carpeta_actual, subcarpeta, archivos_carpeta in os.walk(main_root):
+        for archivo in archivos_carpeta:
+            archivos.append(os.path.join(carpeta_actual, archivo))
+    return archivos
+
+def tamaño_archivo(archivos):
+    tamaños = []
+    for archivo in archivos:
+        tamaño = os.path.getsize(archivo)
+        tamaños.append(tamaño)
+    return tamaños
+
+def obtener_dates(archivos):
+    date_archivos = []
+    for archivo in archivos:
+        fecha_modificacion = os.path.getmtime(archivo)
+        date_archivos.append(datetime.utcfromtimestamp(fecha_modificacion).strftime('%d/%m/%Y %H:%M:%S'))
+    return date_archivos
+
+all_archivos = lista_archivos(main_root)
+archivo_pesos = tamaño_archivo(all_archivos)
+date_archivos = obtener_dates(all_archivos)
+
+archivo_pd = pd.DataFrame({'Archivos': all_archivos, 'Size': archivo_pesos, 'Fechas': date_archivos})
+
+print(archivo_pd)
+
+archivo_pd.to_csv('/workspaces/exercise-terminal-challenge/src/tabla.csv', index= False)
+
+
+
 # Opcional: Hacer lo mismo que en la línea anterior pero en Bash Scripting y exportando un CSV
+
+main_root='/workspaces/exercise-terminal-challenge'
+
--- src/main.sh
+++ src/main.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+main_root='/workspaces/exercise-terminal-challenge'
 
+echo "File, Size, Date" > archivos.csv
+# Recorre el directorio y subdirectorios
+find "$main_root" -type f | while read -r file; do
+    # da nombre del archivo
+    file_name=$(basename "$file")
+    # da tamaño del archivo
+    file_size=$(stat -c %s "$file")
+    # da la fecha de modificación formateada en un formato legible
+    file_date=$(date -d "@$(stat -c %Y "$file")" "+%d/%m/%Y %H:%M:%S")
+    # añade informacion a archivo csv
+    echo "$file_name,$file_size,$file_date" >> archivos.csv
+done
+
+echo "CSV exported to .csv files"
